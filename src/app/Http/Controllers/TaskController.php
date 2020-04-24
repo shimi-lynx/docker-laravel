9701706d1e62e82cf4ec0441cc800c9d1a98eb11
shimi-lynx <n33etc@gmail.com>
2020-04-24 18:03:04 +0900

    feat: フォルダ一覧機能作成

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(int $id)
     {
-        return "Hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+        ]);
+    }
+
+    public function hoge()
+    {
+        return view('tasks/hoge', [
+            'hoge' => 'Hello World!!!'
+        ]);
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/tasks/hoge', 'TaskController@hoge')->name('tasks.hoge');
