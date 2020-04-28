057b7c64000daa3dcdbd2beafb68c4023049c2c4
shimi-lynx <n33etc@gmail.com>
2020-04-28 17:50:07 +0900

    feat: フォルダ作成機能の追加
    - フォルダ作成画面の作成
    - ルーティング設定
    - フォルダ作成機能追加
    - DB側にバリデーション追加
    - フォーム入力値が不正な場合エラーメッセージを返す

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,3 +13,7 @@
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/tasks/hoge', 'TaskController@hoge')->name('tasks.hoge');
+
+// create folcers
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
