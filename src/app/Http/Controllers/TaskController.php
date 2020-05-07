f61b0c38d6c739fd30958e47475c69342a62c1f8
shimi-lynx <n33etc@gmail.com>
2020-05-07 17:58:31 +0900

    feat: タスク作成機能の追加
    - タスク作成機能の追加
    - タスク編集機能の追加
    - テストコードの作成

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -33,6 +34,33 @@ class TaskController extends Controller
     {
         return view('tasks/create', [
             'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+    
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+    
+        $current_folder->tasks()->save($task);
+    
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
         ]);
     }
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -21,3 +21,7 @@ Route::post('/folders/create', 'FolderController@create');
 // create tasks
 Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+// edit tasks
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
